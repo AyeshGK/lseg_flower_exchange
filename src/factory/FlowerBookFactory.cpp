2533c4ad0d3eabc6486b443e844e51125429d823
Ayesh Vininda <ayeshgk@gmail.com>
2023-12-17 17:50:40 +0530

    engine changed

--- src/factory/FlowerBookFactory.cpp
+++ src/factory/FlowerBookFactory.cpp
@@ -11,55 +11,55 @@
 #include "../../include/service/OldMatchingEngine.h"
 
 
-std::shared_ptr<Book> FlowerBookFactory::getFlowerBook(const std::string& instrument) {
-    // Check if a flower book for the instrument type already exists
-    auto it = flowerBooks.find(instrument);
-
-    // If it doesn't exist, create it
-    if (it == flowerBooks.end()) {
-
-        if(instrument == "Rose")
-        {   
-            flowerBooks[instrument] = std::make_shared<RoseBook>(std::make_shared<NewMatchingEngine>());
-        }
-        else if(instrument == "Lavender")
-        {
-            flowerBooks[instrument] = std::make_shared<LavenderBook>(std::make_shared<NewMatchingEngine>());
-        }else if(instrument == "Lotus"){
-            flowerBooks[instrument] = std::make_shared<LotusBook>(std::make_shared<NewMatchingEngine>());
-        }else if(instrument == "Tulip"){
-            flowerBooks[instrument] = std::make_shared<TulipBook>(std::make_shared<NewMatchingEngine>());
-        }else if(instrument == "Orchid"){
-            flowerBooks[instrument] = std::make_shared<OrchidBook>(std::make_shared<NewMatchingEngine>());
-        }
-    }  
-    return flowerBooks[instrument];
-}
-
-
-
 // std::shared_ptr<Book> FlowerBookFactory::getFlowerBook(const std::string& instrument) {
 //     // Check if a flower book for the instrument type already exists
 //     auto it = flowerBooks.find(instrument);
 
 //     // If it doesn't exist, create it
 //     if (it == flowerBooks.end()) {
-//         std::shared_ptr<MatchingEngine> matchingEngine = std::make_shared<NewMatchingEngine>();
 
-//         if (instrument == "Rose") {
-//             flowerBooks[instrument] = std::make_shared<RoseBook>(matchingEngine);
-//         } else if (instrument == "Lavender") {
-//             flowerBooks[instrument] = std::make_shared<LavenderBook>(matchingEngine);
-//         } else if (instrument == "Lotus") {
-//             flowerBooks[instrument] = std::make_shared<LotusBook>(matchingEngine);
-//         } else if (instrument == "Tulip") {
-//             flowerBooks[instrument] = std::make_shared<TulipBook>(matchingEngine);
-//         } else if (instrument == "Orchid") {
-//             flowerBooks[instrument] = std::make_shared<OrchidBook>(matchingEngine);
+//         if(instrument == "Rose")
+//         {   
+//             flowerBooks[instrument] = std::make_shared<RoseBook>(std::make_shared<NewMatchingEngine>());
 //         }
-//     }
-
+//         else if(instrument == "Lavender")
+//         {
+//             flowerBooks[instrument] = std::make_shared<LavenderBook>(std::make_shared<NewMatchingEngine>());
+//         }else if(instrument == "Lotus"){
+//             flowerBooks[instrument] = std::make_shared<LotusBook>(std::make_shared<NewMatchingEngine>());
+//         }else if(instrument == "Tulip"){
+//             flowerBooks[instrument] = std::make_shared<TulipBook>(std::make_shared<NewMatchingEngine>());
+//         }else if(instrument == "Orchid"){
+//             flowerBooks[instrument] = std::make_shared<OrchidBook>(std::make_shared<NewMatchingEngine>());
+//         }
+//     }  
 //     return flowerBooks[instrument];
 // }
+
+
+
+std::shared_ptr<Book> FlowerBookFactory::getFlowerBook(const std::string& instrument) {
+    // Check if a flower book for the instrument type already exists
+    auto it = flowerBooks.find(instrument);
+
+    // If it doesn't exist, create it
+    if (it == flowerBooks.end()) {
+        std::shared_ptr<MatchingEngineBase> matchingEngine = std::make_shared<NewMatchingEngine>();
+
+        if (instrument == "Rose") {
+            flowerBooks[instrument] = std::make_shared<RoseBook>(matchingEngine);
+        } else if (instrument == "Lavender") {
+            flowerBooks[instrument] = std::make_shared<LavenderBook>(matchingEngine);
+        } else if (instrument == "Lotus") {
+            flowerBooks[instrument] = std::make_shared<LotusBook>(matchingEngine);
+        } else if (instrument == "Tulip") {
+            flowerBooks[instrument] = std::make_shared<TulipBook>(matchingEngine);
+        } else if (instrument == "Orchid") {
+            flowerBooks[instrument] = std::make_shared<OrchidBook>(matchingEngine);
+        }
+    }
+
+    return flowerBooks[instrument];
+}
 
 // path : src/factory/FlowerBookFactory.h
